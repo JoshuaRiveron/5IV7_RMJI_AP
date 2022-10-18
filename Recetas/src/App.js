71c359a490a7520971e09c1ce7479a1844ed03d5
JoshuaRiveron <jriveronm2000@alumno.ipn.mx>
2022-10-18 13:48:39 -0500

    Recetas

---
+++ Recetas/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Cards from "./components/Cards";
+
+function App() {
+  <h1>¿Qué receta irás a preparar?</h1>
+  return (
+    <div className="App">
+      <Cards />
+    </div>
+  );
+}
+
+export default App;
